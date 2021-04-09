4532973b9bec7dd09d9ce0757e861dfcc112ca38
Mou Huang <ml.huangsd@gmail.com>
2021-04-08 17:57:54 -0700

    fixed up UI for ItemsAdapter

--- app/src/main/java/codingsharks/ezpricer/fragments/CompareFragment.java
+++ app/src/main/java/codingsharks/ezpricer/fragments/CompareFragment.java
@@ -67,7 +67,7 @@ public class CompareFragment extends Fragment{
         pImage = view_main.findViewById(R.id.productImage);
 
         itemET = view_main.findViewById(R.id.searchbox);
-        itemET.setOnKeyListener(new View.OnKeyListener() {
+        itemET.setOnKeyListener (new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 // If the event is a key-down event on the "enter" button
