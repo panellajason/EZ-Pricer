bcfa141230733e222cf5a37341d24d4a3ae65843
Tristen Seng <42958046+tristenseng@users.noreply.github.com>
2021-04-12 00:39:20 -0700

    Delete EZ-Pricer-master directory

--- EZ-Pricer-master/app/src/main/java/codingsharks/ezpricer/random/EmailConfig.java
+++
@@ -1,7 +0,0 @@
-package codingsharks.ezpricer.random;
-
-public class EmailConfig {
-    // This is our email that we will use to send email. [Sender]
-    public static final String EMAIL = "codingsharks2021@gmail.com";
-    public static final String PASSWORD = "!Codingsharks2021";
-}
